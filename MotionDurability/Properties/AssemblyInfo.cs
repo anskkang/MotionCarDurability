b76928af2cb7c40bc4bdafeb9ea15891c38fedf5
anskkang <kirang.kang@ansys.com>
2023-02-07 18:08:43 +0900

    Task 778450 When the number of modal coordinates is 50, the line changer is operated incorrectly. Every time, one line is blank.

--- MotionDurability/Properties/AssemblyInfo.cs
+++ MotionDurability/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("23.1.22270.0")]
-[assembly: AssemblyFileVersion("23.1.22270.0")]
+[assembly: AssemblyVersion("23.2.23037.0")]
+[assembly: AssemblyFileVersion("23.2.03037.0")]
